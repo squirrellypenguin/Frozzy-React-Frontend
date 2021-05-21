ec6a5d280b81378761d6a153f176c5dea70de3af
Ted Teah <from@dabronx.nyc>
2021-05-21 17:28:14 -0400

    map and reducer for ice cream

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from './pages/main'
 import Shop from './pages/shop'
 import Nav from './components/nav'
 import React from "react"
-
+import Creem from "./pages/icecream"
 function App() {
   
   return (
@@ -20,6 +20,9 @@ function App() {
         </Route>
         <Route path="/checkout">
           <Checkout />
+        </Route>
+        <Route path="/icecream">
+          <Creem />
         </Route>
       </Switch>
       <h1>Frozzy</h1>
