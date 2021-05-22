e0d585ce893ef9f88abf080589da216bae935054
Cody Durham <codydd@gmail.com>
2021-05-21 18:04:45 -0600

    Add ice cream name / star styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router} from 'react-router-dom' 
+import { BrowserRouter as Router} from 'react-router-dom'
+
+
 
 ReactDOM.render(
   <Router>
@@ -18,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
