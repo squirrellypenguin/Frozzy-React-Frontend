d30747e90dd565b3c5aba52e84240b60c841fec3
luis armendariz <luisarmendariz@luiss-MacBook-Pro.local>
2021-05-22 15:39:32 -0600

    Update App.js and Main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const Main = (props) => {
-  return <h1>This is the Main Component</h1>;
+
+  const { shops } = props
+
+  
+  return (
+   <div>
+      <h1>Frizzy</h1>
+      <Link to="/shops">
+      <button>See Shops</button>
+      </Link>
+   </div> 
+  
+  )
 };
 
 export default Main;
