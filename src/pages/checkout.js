5122d9a9261aaaa47c3485815e6e2b23fc4adc26
Ted Teah <from@dabronx.nyc>
2021-05-25 06:45:17 -0400

    add cost counter t checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,29 @@
-import { props } from 'bluebird'
+// import { props } from 'bluebird'
 import React from 'react'
+import {useState} from "react"
 
 const Checkout = (props) => {
 
+    const [totalCost, setTotalCost] = useState([null])
+
     const { cart, removeFromCart } = props
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let cost
+    let value1 = 0
+    let value = [] 
     
+    if (cart !== [])
+    {for (cost in cart) {
+        value1 = cart[cost].cost
+        value.push(cart[cost].cost)
+        console.log(value)   
+    }
+    
+}
+    // console.log(value.reduce(reducer))
 
 const loaded = () => (
+
     <div style={{textAlign: "center"}}>
         {cart.map((flavor, index) => (
             <article key={flavor.id}>
@@ -18,14 +35,14 @@ const loaded = () => (
                 }}>Remove</button>   
             </article>
         ))}
-
+  <h3> TOTAL COST: ETH {value.reduce(reducer)}</h3>
     </div>
 )
 
 const loading = () => <h1>Add your favorite flavors</h1>
 
 return cart.length > 0 ? loaded() : loading()
-  
+
 
 }
 
