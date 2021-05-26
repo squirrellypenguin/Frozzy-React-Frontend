8a29af6c84ac76a268f832ac99d296a6e78e1493
Cody Durham <codydd@gmail.com>
2021-05-26 11:34:42 -0600

    Add styling and routes to footer buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,27 +9,21 @@ const Footer = () => {
         <nav>
             <div id='footer-container'>
             <section class='footer-buttons'>
-                <Link>
-                    <button to='/menu' class='footer-icon'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621611905/Frozzy%20Project/Menu_Icon-01_ofz7cq.png'/></button>
-                    <p class='footer-copy'>Menu</p>
+                <Link to='/checkout'>
+                    <button class='footer-icon'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621613063/Frozzy%20Project/Shpping_Cart_RED_Icon-01_tfsykv.png'/></button>
                 </Link>
+                    <p class='footer-copy'>Cart</p>
             </section>
-
-
-
             <section id='footer-home-button-container'>
-                <Link>
-                    <button to='/' id='home-button'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621617903/Frozzy%20Project/Home_Icon-01-01_hxgeqd.png'/></button>
+                <Link to='/'>
+                        <button id='home-button'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621617903/Frozzy%20Project/Home_Icon-01-01_hxgeqd.png'/></button>
                 </Link>
             </section>
-
-
-
             <section class='footer-buttons'>
-                <Link>
-                    <button to='/menu' class='footer-icon'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621613021/Frozzy%20Project/Profile_Icon-01_a7n7yr.png'/></button>
-                    <p class='footer-copy'>Profile</p>
+                <Link to='/user'>
+                    <button class='footer-icon'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621613021/Frozzy%20Project/Profile_RED_Icon-01_zitgel.png'/></button>
                 </Link>
+                    <p class='footer-copy'>Profile</p>
             </section>
             </div>
         </nav>
