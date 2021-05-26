de374a81c9a0d2f0160336d1c0aa4e5eecd4211a
tedt <from@dabronx.nyc>
2021-05-26 14:01:52 -0400

    addeded missing import for footer with syntax right

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useEffect} from "react"
-import Footer from (../components/Footer)
+import Footer from '../components/Footer'
 const Shop = (props) => {
   // let [shops, setShops] = useState([])
   // let url = "https://frozzybe.herokuapp.com/store/"
