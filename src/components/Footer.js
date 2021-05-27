bc7643a3596f1980b5d81bf376ee6f0a793de079
Ted Teah <from@dabronx.nyc>
2021-05-27 08:33:05 -0400

    change home button so it points to shop instead of the landing page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                     <p class='footer-copy'>Cart</p>
             </section>
             <section id='footer-home-button-container'>
-                <Link to='/'>
+                <Link to='/main/shop'>
                         <button id='home-button'><img src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621617903/Frozzy%20Project/Home_Icon-01-01_hxgeqd.png'/></button>
                 </Link>
             </section>
