02b1323eb8bea5f119beabf3044d708df5ada312
Ted Teah <from@dabronx.nyc>
2021-05-27 08:43:00 -0400

    PS forgot

---
+++ src/pages/landing.js
@@ -0,0 +1,28 @@
+import {Link} from "react-router-dom"
+
+const Main = () => {
+
+
+  
+  return (
+    <div id='black-background'>
+      <section id='main-image-container'>
+        <img id='main-background' src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621658976/Frozzy%20Project/HomePageBackgroundai-01-01_u6jqgs.png'/>
+      </section>
+      <div id='main-logo-container'>
+        <img id='main-logo' src='https://res.cloudinary.com/dhad6e9gj/image/upload/v1621616748/Frozzy%20Project/Frozzy_Logo-01_mbjtw3.png'/>
+      </div>
+      <article id='main-greeting-container'>
+        <p id='main-greeting'>Discover the best flavored ice-cream from over 1000 ice-cream parlours and fast delivery to your doorstep</p>
+      </article>
+      <div id='main-login-button-container'>
+      
+      <Link to='/main/shop'>
+        <button id='main-login-button'>Get frozen now!</button>
+      </Link>
+      </div>
+    </div>
+  )
+};
+
+export default Main;
